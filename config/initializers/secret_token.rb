1b55cb1b0c7c08982de6f5b8a7d315c93fd92e63
何旭欽 <kitecloud213@gmail.com>
2013-09-04 08:13:34 +0800

    Ruby on Rails started!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BetSomething::Application.config.secret_key_base = '811d934876c6d6e00f3cf52dbc9d4aadfb98331d4cc4ca3d4ea060f4ac4de94dc9ecd31eab4312d44b1dfd66511bcf785a8f333adbf0367fa138a24f5564bf03'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BetSomething::Application.config.session_store :cookie_store, key: '_bet_something_session'
